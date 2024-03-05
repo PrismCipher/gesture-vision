abaa1d573daeab85b64d3abd67cc3cd1eeb4ca13
PrismCipher <48163567+PrismCipher@users.noreply.github.com>
2024-03-05 12:51:32 +0200

    Rel-version update

--- gesture-vision/main.py
+++ gesture-vision/main.py
@@ -14,41 +14,30 @@ def calculate_finger_states(hand_landmarks):
     - Список, який представляє стан кожного пальця (0 або 1).
     - Загальна кількість піднятих пальців.
     """
-    finger_states = [0, 0, 0, 0, 0]
-    if hand_landmarks.landmark[2].x > hand_landmarks.landmark[4].x:  # Великий
-        finger_states[0] = 1
-    if hand_landmarks.landmark[4].x < hand_landmarks.landmark[20].x:
-        finger_states[0] = hand_landmarks.landmark[4].x < hand_landmarks.landmark[2].x
-
-    else:
-        finger_states[0] = hand_landmarks.landmark[4].x > hand_landmarks.landmark[2].x
-
-    if hand_landmarks.landmark[6].y > hand_landmarks.landmark[8].y:
-        finger_states[1] = 1
-
-    if hand_landmarks.landmark[10].y > hand_landmarks.landmark[12].y:  # середній
-        finger_states[2] = 1
-
-    if hand_landmarks.landmark[14].y > hand_landmarks.landmark[16].y:
-        finger_states[3] = 1
-
-    if hand_landmarks.landmark[18].y > hand_landmarks.landmark[20].y:
-        finger_states[4] = 1
-
-    fingers_count = sum(finger_states)
+    landmarks = np.array([(landmark.x, landmark.y) for landmark in hand_landmarks.landmark])
+    finger_states = np.zeros(5, dtype=int)
+    
+    # Thumb
+    finger_states[0] = int(landmarks[2, 0] > landmarks[4, 0])
+    
+    # Other fingers
+    finger_states[1:] = (landmarks[[6, 10, 14, 18], 1] > landmarks[[8, 12, 16, 20], 1]).astype(int)
+    
+    fingers_count = np.sum(finger_states)
 
     return finger_states, fingers_count
 
+
 def unblur_face(img, face_detection):
     """
-    Убрать размытие с лиц на изображении.
+    Прибрати розмиття з облич на зображенні.
 
-    Аргументы:
-    - img: Входное изображение.
-    - face_detection: Модель обнаружения лиц.
+    Аргументи:
+    - img: Вхідне зображення.
+    - face_detection: Модель виявлення облич.
 
-    Возвращает:
-    - Изображение с не размытыми лицами.
+    Повертає:
+    - Зображення із нерозмитими обличчями.
     """
     H, W, _ = img.shape
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -67,20 +56,8 @@ def unblur_face(img, face_detection):
                     img[y1:y1 + h, x1:x1 + w, :] = face_region
 
     return img
-def process_image_with_detection(img, face_detection, hands_detection, blur_enabled=True):
-    """
-    Обробка зображення шляхом виявлення обличчя та рук, та застосування розмиття на основі жестів рук.
-
-    Аргументи:
-    - img: Вхідне зображення.
-    - face_detection: Модель виявлення облич.
-    - hands_detection: Модель виявлення рук.
-    - blur_enabled: Булеве значення, що вказує, чи застосовувати розмиття.
-
-    Повертає:
-    - Оброблене зображення.
-    - Поточний стан розмиття.
-    """
+def process_image_with_detection(img, hands_detection, blur_enabled=True):
+    #Функція розмиття руки
 
     H, W, _ = img.shape
 
@@ -113,17 +90,17 @@ def process_image_with_detection(img, face_detection, hands_detection, blur_enab
     return img, blur_enabled
 def process_img(img, face_detection, hands_detection, blur_enabled=True):
     """
-    Обработка изображения путем обнаружения лиц и рук, и применения размытия на основе жестов рук.
+    Обробка зображення шляхом виявлення облич та рук, та застосування розмиття на основі жестів рук.
 
-    Аргументы:
-    - img: Входное изображение.
-    - face_detection: Модель обнаружения лиц.
-    - hands_detection: Модель обнаружения рук.
-    - blur_enabled: Булево значение, указывающее, применять ли размытие.
+    Аргументи:
+    - img: Вхідне зображення.
+    - face_detection: Модель виявлення облич.
+    - hands_detection: Модель виявлення рук.
+    - blur_enabled: Бульове значення, що вказує, чи застосовувати розмиття.
 
-    Возвращает:
-    - Обработанное изображение.
-    - Текущее состояние размытия.
+    Повертає:
+    - Оброблене зображення.
+    - Поточний стан розмиття.
     """
     H, W, _ = img.shape
 
@@ -169,62 +146,65 @@ def process_img(img, face_detection, hands_detection, blur_enabled=True):
                 elif fingers_count == 2 and finger_states[1]==1 and finger_states[2] ==1:
                     blur_enabled = False
                 elif fingers_count == 1 and finger_states[1] == 1:
-                    process_image_with_detection(img, face_detection, hands_detection, blur_enabled=True)
+                    process_image_with_detection(img, hands_detection)
 
 
 
     return img, blur_enabled
 
-output_dir = './output'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+def check_output_dir(output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-mp_face_detection = mp.solutions.face_detection
-with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
-    mp_hands = mp.solutions.hands
-    with mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5) as hands_detection:
+def main():
+    mp_face_detection = mp.solutions.face_detection
+    with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+        mp_hands = mp.solutions.hands
+        with mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5) as hands_detection:
 
-        args = {"mode": 'webcam', "filePath": None}
+            args = {"mode": 'webcam', "filePath": None}
+            output_dir = './output'
+            if args["mode"] in ["image"]:
+                img = cv2.imread(args["filePath"])
+                check_output_dir(output_dir)
+                img = process_img(img, face_detection, hands_detection)
 
-        if args["mode"] in ["image"]:
-            img = cv2.imread(args["filePath"])
+                cv2.imwrite(os.path.join(output_dir, 'output.png'), img)
 
-            img = process_img(img, face_detection, hands_detection)
+            elif args["mode"] in ['video']:
+                cap = cv2.VideoCapture(args["filePath"])
+                ret, frame = cap.read()
+                check_output_dir(output_dir)
+                output_video = cv2.VideoWriter(os.path.join(output_dir, 'output.mp4'),
+                                            cv2.VideoWriter_fourcc(*'MP4V'),
+                                            25,
+                                            (frame.shape[1], frame.shape[0]))
 
-            cv2.imwrite(os.path.join(output_dir, 'output.png'), img)
+                while ret:
+                    frame = process_img(frame, face_detection, hands_detection)
 
-        elif args["mode"] in ['video']:
-            cap = cv2.VideoCapture(args["filePath"])
-            ret, frame = cap.read()
+                    output_video.write(frame)
 
-            output_video = cv2.VideoWriter(os.path.join(output_dir, 'output.mp4'),
-                                           cv2.VideoWriter_fourcc(*'MP4V'),
-                                           25,
-                                           (frame.shape[1], frame.shape[0]))
+                    ret, frame = cap.read()
 
-            while ret:
-                frame = process_img(frame, face_detection, hands_detection)
+                cap.release()
+                output_video.release()
 
-                output_video.write(frame)
+            elif args["mode"] in ['webcam']:
+                cap = cv2.VideoCapture(0)
 
                 ret, frame = cap.read()
+                blur_enabled = True
+                while ret:
+                    frame, blur_enabled = process_img(frame, face_detection, hands_detection, blur_enabled)
 
-            cap.release()
-            output_video.release()
+                    cv2.imshow('frame', frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
 
-        elif args["mode"] in ['webcam']:
-            cap = cv2.VideoCapture(0)
+                    ret, frame = cap.read()
+                cv2.destroyAllWindows()
+                cap.release()
 
-            ret, frame = cap.read()
-            blur_enabled = True
-            while ret:
-                frame, blur_enabled = process_img(frame, face_detection, hands_detection, blur_enabled)
-
-                cv2.imshow('frame', frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-
-                ret, frame = cap.read()
-
-            cap.release()
-
+if __name__ == "__main__":
+    main()
